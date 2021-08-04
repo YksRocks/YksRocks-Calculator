119eb20ea305b77bbaec5303d4deb734e41c0466
YksRocks <yk66478@gmail.com>
2021-08-04 14:05:12 +0530

    .

--- theme.js
+++ theme.js
@@ -1,36 +1,34 @@
-const circle = document.querySelector('.hello2');
-const body = document.querySelector('body');
-const calc = document.querySelector('.calc');
-const th = document.querySelector('.th');
-const  resultt= document.querySelector('.result');
-const  numberss= document.querySelector('.numbers');
-const  buttonn= document.querySelectorAll('button');
-const  del= document.querySelector('.del');
-const  rsb= document.querySelector('.resetbtn');
-const  eb= document.querySelector('.equalbtn');
+const circle = document.querySelector(".hello2");
+const body = document.querySelector("body");
+const calc = document.querySelector(".calc");
+const th = document.querySelector(".th");
+const resultt = document.querySelector(".result");
+const numberss = document.querySelector(".numbers");
+const buttonn = document.querySelectorAll("button");
+const del = document.querySelector(".del");
+const rsb = document.querySelector(".resetbtn");
+const eb = document.querySelector(".equalbtn");
 
 const stylee = document.getElementById("stylee");
 
-
-const theme1 = document.querySelector('.one')
+const theme1 = document.querySelector(".one");
 theme1.addEventListener("click", function () {
-    circle.classList.remove('colorChange');
-    circle.classList.remove('colorChange3');
-    stylee.setAttribute('href', 'css/theme1/dark_theme.css');
-})
+  circle.classList.remove("colorChange");
+  circle.classList.remove("colorChange3");
+  stylee.setAttribute("href", "css/theme1/dark_theme.css");
+});
 
-const theme2 = document.querySelector('.two');
+const theme2 = document.querySelector(".two");
 theme2.addEventListener("click", function () {
-    circle.classList.add('colorChange');
-    circle.classList.remove('colorChange3');
-    
-    stylee.setAttribute('href', 'css/theme2/white_theme.css');
-})
+  circle.classList.add("colorChange");
+  circle.classList.remove("colorChange3");
 
+  stylee.setAttribute("href", "css/theme2/white_theme.css");
+});
 
-const theme3 = document.querySelector('.three')
+const theme3 = document.querySelector(".three");
 theme3.addEventListener("click", function () {
-    circle.classList.add('colorChange3');
-    circle.classList.remove('colorChange');
-    stylee.setAttribute('href', 'css/theme3/purple_theme.css');
-})
+  circle.classList.add("colorChange3");
+  circle.classList.remove("colorChange");
+  stylee.setAttribute("href", "css/theme3/purple_theme.css");
+});
